444ae28994bb38d470c03b43e55802993d091784
brycecr <brycecr@gmail.com>
2014-05-14 11:03:32 -0700

    adding imports

--- jsfun/Weather App/weather.js
+++ jsfun/Weather App/weather.js
@@ -8,7 +8,12 @@ var app = new WeatherApp();
 // Your main function for the weather app.  You should create a new WeeatherApp object
 // add all buttons and controls to the screen in this function.
 function run() {
-	
+	app.addButton("Show Weather");
+    app.addButton("Local Weather");
+    app.addTextField();
+    app.addCanvas();
+    var rect = new GCircle(50,50,50);
+    add(rect);
 }
 
 
@@ -16,7 +21,8 @@ function run() {
 // Should process the user's query and make a request to the appropriate weather API to fetch
 // the weather for the given location.
 function showWeather(query) {
-
+    var weather = app.fetchWeatherForQuery(query, 7, success, error);
+    console.log(weather);
 }
 
 
@@ -24,7 +30,8 @@ function showWeather(query) {
 // Should process the user's current location and make a call to the appropriate weather API
 // to fetch the weather for the user's current location.
 function showLocalWeather(latitude, longitude) {
-	
+    var weather = app.fetchWeatherForCoordinates(latitude, longitude, 7, success, error);
+    console.log(weather);
 }
 
 /********************************************************/
