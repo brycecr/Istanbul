577a75a68b097127a6f2ccfb4997b979420eda17
brycecr <brycecr@gmail.com>
2014-05-20 21:10:58 -0700

    moving thoung around, emptying randomcircles starter code

--- jsfun_final/randomCircles/randomCircles.js
+++ jsfun_final/randomCircles/randomCircles.js
@@ -1,24 +1,3 @@
-var app = new GraphicsApp();
-
-var HEIGHT = 400;
-var WIDTH = 400;
 
 function run() {
-    app.addButton("Make Circle!", randomCircle);
-    app.addButton("Clear Canvas", clearCircles);
-    app.addCanvas(WIDTH, HEIGHT);
-}
-
-function clearCircles() {
-    clearCanvas();
-}
-
-function randomCircle() {
-    var randx = Math.random()*WIDTH;
-    var randy = Math.random()*HEIGHT;
-    var randwidth = Math.random() * 100 + 50;
-    var randheight = Math.random() * 100 + 50;
-    var oval = new GOval(randx, randy, randwidth, randheight);
-    oval.setColor(pickRandomProperty(Color));
-    add(oval);
 }
--- jsfun_legacy/weather_app/weather.js
+++
@@ -1,4 +0,0 @@
-// Your main function for the weather app.
-function run() {
-	
-}
