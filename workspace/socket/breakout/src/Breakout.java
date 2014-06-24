e6968a445698dc06a19403fd626cc8598dea0e78
Te-Yuan Huang <huangty@Te-Yuans-MacBook-Air.local>
2014-06-24 18:01:01 +0300

    socket programming - 2 person breakout

---
+++ workspace/socket/breakout/src/Breakout.java
@@ -0,0 +1,280 @@
+// TODO: comment this program
+
+import acm.graphics.*;     // GOval, GRect, etc.
+import acm.io.IODialog;
+import acm.program.*;      // GraphicsProgram
+import acm.util.*;         // RandomGenerator
+
+import java.applet.*;      // AudioClip
+import java.awt.*;         // Color
+import java.awt.event.*;   // MouseEvent
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.*;
+
+public class Breakout extends GraphicsProgram implements BreakoutConstants {
+	int NUM_LIFE = 1000000;
+	GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+	GOval ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+	GLabel nameTag;
+	ArrayList<GLabel> scores = new ArrayList<GLabel>();
+	String userName;
+	String serverName;
+	Socket breakoutSocket;
+	BufferedReader in;
+	public static ArrayList<String> msgQueue = new ArrayList<String>();
+
+	public void run() {
+		setUp();
+		addMouseListeners();
+		
+		for(int i = 0; i < NUM_LIFE; i++) {
+			resetBall();
+			//waitForClick();
+			playTurn();
+		}
+	}
+
+	private void setUp() {
+		getUserInput();
+		establishConn();
+		drawNameTag();
+		drawBricks();
+		paddle.setFilled(true);
+		add(paddle, 0, getHeight() - PADDLE_Y_OFFSET);
+		ball.setFilled(true);
+	}
+	
+	private void drawNameTag(){
+		nameTag = new GLabel("Player " + userName);
+		nameTag.scale(1.5);
+		add(nameTag, getWidth()/2 - nameTag.getWidth()/2, 20);
+	}
+
+	private void resetBall() {
+		double ballX = (getWidth() - ball.getWidth()) / 2;
+		double ballY = (getHeight() - ball.getHeight()) / 2;
+		add(ball, ballX, ballY);
+	}
+	
+	private void getUserInput(){
+		IODialog nameDialog = new IODialog();
+		userName = nameDialog.readLine("Please Enter Your Name:");
+		IODialog serverDialog = new IODialog();
+		serverName = serverDialog.readLine("Please Enter the IP Address or Hostname of the Breakout Server:");
+	}
+	
+	private void establishConn(){
+		try{
+			breakoutSocket = new Socket(serverName, PORT);
+			in = new BufferedReader(new InputStreamReader(breakoutSocket.getInputStream()));
+		}catch(IOException e){
+			IODialog errDialog = new IODialog();
+			errDialog.showErrorMessage(e.getLocalizedMessage());
+			System.exit(1);
+		}
+		pause(1000);
+		
+		//Successfully connected to the game server
+		String joinMsg = "/join " + userName;
+		sendMsg(joinMsg);
+		
+		String waitMsg = "Wait for the Other Player to Join ... ";
+		GLabel waitScreen = new GLabel(waitMsg);
+		waitScreen.scale(1.5);
+		waitScreen.setFont("Helvetica Bold");
+		add(waitScreen, getWidth()/2 - waitScreen.getWidth()/2, getHeight()/2);
+		
+		String recv;
+		try {
+			while ((recv = in.readLine()) != null) {
+				if(recv.startsWith("/start")){
+					remove(waitScreen);
+					countDown();
+					new commandHandler(in).start();
+					break;
+				}
+				System.out.println("waiting for the start msg, received: " +  recv);
+			}
+		} catch (IOException e) {
+			IODialog errDialog = new IODialog();
+			errDialog.showErrorMessage(e.getLocalizedMessage());
+			System.exit(1);
+		}
+		println("LET's GO!");
+	}
+	
+	private void countDown(){
+		for(int i = 3; i > 0; i--){
+			String waitMsg = i + " ... ";
+			GLabel waitScreen = new GLabel(waitMsg);
+			waitScreen.setFont("Helvetica Bold");
+			waitScreen.scale(5);
+			waitScreen.setColor(Color.RED);
+			add(waitScreen, getWidth()/2 - waitScreen.getWidth()/2, getHeight()/2);
+			pause(1000);
+			remove(waitScreen);
+		}
+	}
+	
+	private void updateWorld(){
+		for(int i = 0; i< msgQueue.size(); i++){
+			String inputLine = msgQueue.remove(0);
+			if(inputLine != null){
+	        	println("MSG received: " + inputLine);
+	        	
+	        	StringTokenizer st = new StringTokenizer(inputLine);
+	        	String command = st.nextToken();
+	        	if(command.contains("/remove")){
+	        		Double x = new Double(st.nextToken());
+	        		Double y = new Double(st.nextToken());
+	        		println("Removing Object at (" + x + ", " + y + ")");
+	        		GObject colliding = getElementAt(x, y);
+	        		if(colliding != null){
+		        		remove(colliding);
+	        		}
+	        	}else if(command.contains("/score")){
+	        		println("score update: " + inputLine);
+	        		while(!scores.isEmpty()){
+	        			remove(scores.remove(0));
+	        		}
+	        		int startX = 0;
+	        		while(st.hasMoreTokens()){
+	        			//Format: TY:2 
+	        			String name_score = st.nextToken();
+	        			GLabel scoreLabel = new GLabel(name_score);
+	        			scoreLabel.scale(1.5);
+	        			add(scoreLabel, startX, nameTag.getHeight()*2);
+	        			startX += scoreLabel.getWidth() + 10;
+	        			scores.add(scoreLabel);
+	        		}
+	        	}
+	        }
+		}
+	}
+	
+	private void sendMsg(String msg){
+		try{
+			PrintWriter out = new PrintWriter(breakoutSocket.getOutputStream(), true);
+			out.println(msg);
+		}catch(IOException e){
+			IODialog errDialog = new IODialog();
+			errDialog.showErrorMessage(e.getLocalizedMessage());
+		}
+	}
+	private void playTurn() {
+		double dx = .5;
+		double dy = .5;
+		while(ball.getY() < getHeight()) {
+			updateWorld();
+			ball.move(dx, dy);
+
+			double ballX = ball.getX();
+			double ballY = ball.getY();
+			if(ballX < 0 || ballX > getWidth() - BALL_RADIUS * 2) {
+				dx *= -1;
+			}
+			
+			if(ballY < 0) {
+				dy *= -1;
+			}
+
+			GObject colliding = getCollidingObject();
+			
+			
+			if(colliding == paddle) {
+				dy = -Math.abs(dy);
+			} else if(colliding != null && colliding != nameTag && !scores.contains(colliding)) {
+				remove(colliding);
+				String msg ="/remove " + colliding.getX() + " " + colliding.getY() + " " + userName;
+				sendMsg(msg);
+				dy *= -1;
+			}
+
+			//sendMsg();
+			//recvMsg();
+			pause(DELAY);
+		}
+	}
+
+	private GObject getCollidingObject() {
+		double ballX = ball.getX();
+		double ballY = ball.getY();
+		double ballDiameter = 2 * BALL_RADIUS;
+		if(getElementAt(ballX, ballY) != null) {
+			return getElementAt(ballX, ballY);
+		}
+		if(getElementAt(ballX + ballDiameter, ballY) != null) {
+			return getElementAt(ballX + ballDiameter, ballY);
+		}
+		if(getElementAt(ballX, ballY + ballDiameter) != null) {
+			return getElementAt(ballX, ballY + ballDiameter);
+		}
+		if(getElementAt(ballX + ballDiameter, ballY + ballDiameter) != null) {
+			return getElementAt(ballX + ballDiameter, ballY + ballDiameter);
+		}
+		return null;
+
+	}
+
+	private void drawBricks() {
+
+		for(int i = 0; i < NBRICK_ROWS; i++) {
+			for(int j = 0; j < NBRICKS_PER_ROW; j++) {
+				double x = BRICK_SEP + j * (BRICK_WIDTH + BRICK_SEP);
+				double y = BRICK_Y_OFFSET + i * (BRICK_HEIGHT + BRICK_SEP);
+				Brick r = new Brick(i, j, BRICK_WIDTH, BRICK_HEIGHT);
+				Color rectColor = getColor(i);
+				r.getRect().setColor(rectColor);
+				r.getRect().setFilled(true);
+				add(r.getRect(), x, y);
+			}
+		}
+
+
+	}
+
+	private Color getColor(int i) {
+		if(i == 0 || i == 1) {
+			return Color.RED;
+		}
+		if(i == 2 || i == 3) {
+			return Color.ORANGE;
+		}
+		if(i == 4 || i == 5) {
+			return Color.YELLOW;
+		}
+		if(i == 6 || i == 7) {
+			return Color.GREEN;
+		}
+		return Color.CYAN;
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		double paddleX = e.getX() - paddle.getWidth() / 2;
+		paddle.setLocation(paddleX, getHeight() - PADDLE_Y_OFFSET);
+	}
+
+	private static class commandHandler extends Thread {
+		private BufferedReader input;
+		public commandHandler(BufferedReader in){
+			this.input = in;
+		}
+		public void run(){
+			String inputLine;
+			try {
+				while ((inputLine = input.readLine()) != null) {
+					synchronized(msgQueue){
+						msgQueue.add(inputLine);
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ workspace/socket/breakout/src/BreakoutServer.java
@@ -0,0 +1,132 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+import acm.io.IODialog;
+
+
+
+public class BreakoutServer implements BreakoutConstants{
+	public static final int MIN_PEOPLE_IN_GAME = 2;
+	public static final int GAME_STATUS_WAITING = 0;
+	public static final int GAME_STATUS_STARTED = 1;
+	public static final int GAME_STATUS_FINISH = 2;
+
+	public static Hashtable<String, PrintWriter> clientSocketWriterMap = new Hashtable<String, PrintWriter>();
+	public static Hashtable<String, Integer> scoreBoard = new Hashtable<String, Integer>();
+	public static String[][] bricks = new String[NBRICK_ROWS][NBRICKS_PER_ROW];
+	public static int GAME_STATUS = GAME_STATUS_WAITING;
+
+	private static class ClientHandler extends Thread {
+		private Socket clientSocket;
+		private PrintWriter out;
+		private BufferedReader in;
+		public ClientHandler(Socket s){
+			clientSocket = s;
+			try {
+				out = new PrintWriter(clientSocket.getOutputStream(), true);
+		        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+		}
+		
+		@Override
+		public void run() {
+			try{
+		        String inputLine;
+		        while ((inputLine = in.readLine()) != null) {
+		        	StringTokenizer st = new StringTokenizer(inputLine);
+		        	String command = st.nextToken();
+		        	
+		        	if(command.startsWith("/join")){
+		        		String clientName = st.nextToken();
+	        			System.out.println("inside the synchronized block");
+	        			if(!clientSocketWriterMap.containsKey(clientName)){
+	        				clientSocketWriterMap.put(clientName, out);
+	        				scoreBoard.put(clientName, 0);
+	        				if(GAME_STATUS == GAME_STATUS_WAITING && clientSocketWriterMap.size() >= MIN_PEOPLE_IN_GAME){
+	        					GAME_STATUS = GAME_STATUS_STARTED;
+	        					broadcast("/start", null);
+	        				}else if(GAME_STATUS == GAME_STATUS_STARTED){
+	        					String update = "update";
+	        					sendMsg(update);
+	        				}else{
+	        					GAME_STATUS = GAME_STATUS_WAITING;
+	        					System.out.println("Waiting for at least " + (MIN_PEOPLE_IN_GAME - clientSocketWriterMap.size()) + "more people to join the game.");
+	        				}
+        					System.out.println( clientSocketWriterMap.size() + " people in the game");
+	        			}else{
+	        				/*handle the problem where an existing client using the same name*/
+	        			}
+		        	}else if(command.startsWith("/remove")){
+		        		String x = st.nextToken();
+		        		String y = st.nextToken();
+		        		String userName = st.nextToken();
+		        		ArrayList<String> exceptList = new ArrayList<String>();
+		        		exceptList.add(userName);
+		        		broadcast(inputLine, exceptList);
+		        		scoreBoard.put(userName, scoreBoard.get(userName) + 1);
+		        		String scoreUpdate = convertScoreBoradtoJSON();
+		        		System.out.println(userName + " remove the brick at ("+ x +", "+ y +") " );
+		        		System.out.println("Updated score: " + scoreUpdate);
+		        		broadcast(scoreUpdate, null);
+		        	}
+		        }
+			}catch(IOException e){
+				System.err.println(e);
+			}
+
+		}
+
+		public String convertScoreBoradtoJSON(){
+			String scoreUpdate = "/score";
+			Iterator<Map.Entry<String, Integer>> it = scoreBoard.entrySet().iterator();
+			while(it.hasNext()){
+				Map.Entry<String, Integer> pairs = (Map.Entry<String, Integer>) it.next();
+				scoreUpdate += " " + pairs.getKey()+":"+pairs.getValue();
+			}
+			return scoreUpdate;
+		}
+		public void broadcast(String msg, ArrayList<String> exceptList){
+			System.out.println("broadcasting ... " + msg);
+			Iterator<Map.Entry<String, PrintWriter>> it = clientSocketWriterMap.entrySet().iterator();
+			while(it.hasNext()){
+				Map.Entry<String, PrintWriter> pairs = (Map.Entry<String, PrintWriter>) it.next();
+				String name = (String)pairs.getKey();
+				PrintWriter clientOut = (PrintWriter) pairs.getValue();
+				if(exceptList!=null && exceptList.contains(name)){
+					System.out.println("Skip sending message to " + name);
+					continue;
+				}
+				clientOut.println(msg);
+				System.out.println("Sending message to ... " + (String) pairs.getKey());
+			}
+		}
+		
+		public void sendMsg(String msg){
+			out.println(msg);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		System.out.println("The breakout server is running.");
+		
+		try {
+			while (true){
+				new ClientHandler(serverSocket.accept()).start();
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		} finally{
+			serverSocket.close();
+		}
+
+	}
+}
